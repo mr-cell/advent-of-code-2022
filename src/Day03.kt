90fc7c29dec4f51806d907b68ce040f59a45675b
mr-cell <marcel.schally@gmail.com>
2022-12-08 20:35:07 +0100

    day 3 solution 1 star

---
+++ src/Day03.kt
@@ -0,0 +1,31 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input
+            .map { Pair(it.substring(0, it.length/2), it.substring(it.length/2)) }
+            .map { it.first.toCharArray().intersect(it.second.toCharArray().toList().toSet()) }
+            .map { it.distinct() }
+            .flatMap {
+                it.map { char ->
+                    return@map if (CharRange('a', 'z').contains(char)) {
+                        char.minus('a') + 1
+                    } else {
+                        char.lowercaseChar().minus('a') + 27
+                    }
+                }
+            }
+            .sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
