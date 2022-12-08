f1ec8c30a029ea7f42afe9a7627c8cdf95bca271
mr-cell <marcel.schally@gmail.com>
2022-12-08 20:44:47 +0100

    day 3 solution 2 star

--- src/Day03.kt
+++ src/Day03.kt
@@ -17,13 +17,26 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return input
+            .map { it.toCharArray() }
+            .windowed(3, 3)
+            .map { it.reduce { acc, chars -> acc.intersect(chars.toList().toSet()).toCharArray()} }
+            .flatMap {
+                it.map { char ->
+                    return@map if (CharRange('a', 'z').contains(char)) {
+                        char.minus('a') + 1
+                    } else {
+                        char.lowercaseChar().minus('a') + 27
+                    }
+                }
+            }
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
