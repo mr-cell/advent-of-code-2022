dc3ef9aee4af2db7f0033320cc57e7667a6dc49d
mr-cell <marcel.schally@gmail.com>
2022-12-12 20:33:52 +0100

    day 7 solution 2 star better

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,44 +1,15 @@
 fun main() {
 
-    fun getDirTree(input: List<String>): Node {
-        val dirTree = Node()
-        var context = Context(currentDir = dirTree)
-
-        input.forEach { row ->
-            if (row.startsWith("$ cd")) {
-                val arg = row.split(" ").last()
-                if (arg == "..") {
-                    context = context.currentDir.parent?.let { Context(it) } ?: context
-                } else if (arg == "/") {
-                    context = Context(dirTree)
-                } else {
-                    context = context.currentDir.children.first { it.name == arg }.let { Context(it) }
-                }
-            } else if (row.startsWith("$ ls")) {
-                // ignore
-            } else if (row.startsWith("dir")) {
-                val arg = row.split(" ").last()
-                context.currentDir.children.add(Node(context.currentDir, arg))
-            } else {
-                val fileInfo = row.split(" ")
-                val fileName = fileInfo.last()
-                val fileSize = fileInfo.first().toInt()
-                context.currentDir.children.add(Node(context.currentDir, fileName, fileSize))
-            }
-        }
-        dirTree.calculateSize()
-        return dirTree
-    }
-
     fun part1(input: List<String>): Int {
-        val dirTree = getDirTree(input)
-        return  dirTree.getAllDirs().map { it.size }.filter { it <= 100000 }.sum()
+        val rootDir = parseInput(input)
+        return rootDir.find { it.size <= 100_000 }.sumOf { it.size }
     }
 
     fun part2(input: List<String>): Int {
-        val dirTree = getDirTree(input)
-        val usedSpace = dirTree.size
-        return dirTree.getAllDirs().sortedBy { it.size }.first { (70000000 - usedSpace) + it.size >= 30000000 }.size
+        val rootDir = parseInput(input)
+        val unusedSpace = 70_000_000 - rootDir.size
+        val deficit = 30_000_000 - unusedSpace
+        return rootDir.find { it.size >= deficit }.minBy { it.size }.size
     }
 
     // test if implementation meets criteria from the description, like:
@@ -51,23 +22,40 @@ fun main() {
     println(part2(input))
 }
 
-private data class Context(val currentDir: Node)
+private fun parseInput(input: List<String>): Directory {
+    val callStack = ArrayDeque<Directory>().apply { add(Directory("/")) }
+    input.forEach { item ->
+        when {
+            item == "$ ls" -> {} // no-op
+            item.startsWith("dir") -> {} // no-op
+            item == "$ cd /" -> callStack.removeIf { it.name != "/" }
+            item == "$ cd .." -> callStack.removeFirst()
+            item.startsWith("$ cd") -> {
+                val name = item.substringAfterLast(" ")
+                callStack.addFirst(callStack.first().traverse(name))
+            }
+            else -> {
+                val size = item.substringBefore(" ").toInt()
+                callStack.first().addFile(size)
+            }
+        }
+    }
+    return callStack.last()
+}
 
-private class Node(val fullPath: String = "/", val parent: Node? = null, var size: Int = 0) {
-    val children = mutableListOf<Node>()
-    val name = fullPath.split("/").lastOrNull { it.isNotBlank() } ?: "/"
+private class Directory(val name: String) {
+    private val children: MutableMap<String, Directory> = mutableMapOf()
+    private var sizeOfFiles: Int = 0
 
-    constructor(parent: Node, name: String, size: Int = 0) : this("${parent.fullPath}${name}/", parent, size)
+    val size: Int
+        get() = sizeOfFiles + children.values.sumOf { it.size }
 
-    fun isLeaf(): Boolean = children.size == 0
-
-    fun calculateSize() {
-        children.forEach { it.calculateSize() }
-        size += children.sumOf { it.size }
+    fun addFile(size: Int) {
+        sizeOfFiles += size
     }
 
-    fun getAllDirs(): List<Node> {
-        val childrenDirs = children.filter { !it.isLeaf() }.flatMap { it.getAllDirs() }
-        return listOf(this, *childrenDirs.toTypedArray())
-    }
+    fun traverse(dir: String): Directory = children.getOrPut(dir) { Directory(dir) }
+
+    fun find(predicate: (Directory) -> Boolean): List<Directory> =
+        children.values.filter(predicate) + children.values.flatMap { it.find(predicate) }
 }
