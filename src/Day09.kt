784949312f1db6ae763405a4bcf33512ede2b128
mr-cell <marcel.schally@gmail.com>
2022-12-15 21:08:40 +0100

    day 9 solution 2 star

--- src/Day09.kt
+++ src/Day09.kt
@@ -6,39 +6,36 @@ fun main() {
         val moves = parseInput(input)
 
         return moves
-            .fold(mutableListOf(Position(0, 0))) { heads, move ->
-                heads.add(heads.last().move(x = move.first, y = move.second))
-                heads
-            }
+            .toHeadPositions()
             .fold(mutableListOf(Position(0, 0))) { tails, head ->
-                val tail = tails.last()
-                val distance = tail.distance(head)
-                val x = if (distance.first == 0) 1  else distance.first / abs(distance.first)
-                val y = if (distance.second == 0) 1 else distance.second / abs(distance.second)
-
-                if ((abs(distance.first) == 2 && abs(distance.second) == 1)
-                    || (abs(distance.first) == 1 && abs(distance.second) == 2)) {
-
-                    tails.add(tail.move(x = x, y = y))
-                } else if (abs(distance.first) == 2) {
-                    tails.add(tail.move(x = x))
-                } else if (abs(distance.second) == 2) {
-                    tails.add(tail.move(y = y))
-                } else {
-                    tails.add(tail.move())
-                }
+                tails.add(tails.last().newKnotPosition(head))
                 tails
             }.distinct().count()
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val moves = parseInput(input)
+
+        val initialKnotPositions = (0 until 9).map { _ -> Position(0, 0) }.toMutableList()
+        return moves
+            .toHeadPositions()
+            .fold(mutableListOf(initialKnotPositions)) { knotsPositions, head ->
+                val lastKnotsPositions = knotsPositions.last()
+                val currentKnotPositions = mutableListOf(lastKnotsPositions[0].newKnotPosition(head))
+                knotsPositions.add(currentKnotPositions)
+                (1 until 9).map {
+                    knotsPositions.last().add(lastKnotsPositions[it].newKnotPosition(currentKnotPositions[it - 1]))
+                }
+                knotsPositions
+            }.map { it.last() }.distinct().count()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
+    val testInput2 = readInput("Day09_test_2")
     check(part1(testInput) == 13)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 1)
+    check(part2(testInput2) == 36)
 
     val input = readInput("Day09")
     println(part1(input))
@@ -58,5 +55,30 @@ private fun parseInput(input: List<String>): List<Move> =
                 }
             }
         }
+
+private fun List<Move>.toHeadPositions(): List<Position> {
+    return this.fold(mutableListOf(Position(0, 0))) { headPositions, move ->
+        headPositions.add(headPositions.last().move(x = move.first, y = move.second))
+        headPositions
+    }
+}
+
+private fun Position.newKnotPosition(precedingKnotPosition: Position): Position {
+    val distance = this.distance(precedingKnotPosition)
+    val x = if (distance.first == 0) 1  else distance.first / abs(distance.first)
+    val y = if (distance.second == 0) 1 else distance.second / abs(distance.second)
+
+    return if ((abs(distance.first) == 2 && abs(distance.second) >= 1)
+        || (abs(distance.first) >= 1 && abs(distance.second) == 2)) {
+
+        this.move(x = x, y = y)
+    } else if (abs(distance.first) == 2) {
+        this.move(x = x)
+    } else if (abs(distance.second) == 2) {
+        this.move(y = y)
+    } else {
+        this.move()
+    }
+}
 
 typealias Move = Pair<Int, Int>
